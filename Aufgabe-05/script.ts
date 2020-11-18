91d365607f260806165c70acbe196168a4f2b487
tschuulez <72871233+tschuulez@users.noreply.github.com>
2020-11-18 14:01:43 +0100

    again

--- Aufgabe-05/script.ts
+++ Aufgabe-05/script.ts
@@ -18,33 +18,33 @@ var australia18: number = 2100.5;
 
 var total: number = africa18 + southAmerica18 + europe18 + northAmerica18 + asia18 + australia18; 
 
-console.log("Africa produces" + africa18 + "kg CO2")
-console.log("Africa contributes relatively" + Math.round(africa18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("Africa's emission from 2018 has increased by" + Math.round(africa18/africa08*100)  + "since 2008")
-console.log("That is a growth rate of approximatley" + (africa18-africa08) + "kg CO2")
+console.log("Africa produces " + africa18 + "kg CO2")
+console.log("Africa contributes relatively " + Math.round(africa18/total*100) + "% Carbon Dioxide to the world's total emission")
+console.log("Africa's emission from 2018 has increased by " + Math.round(africa18/africa08*100)  + " since 2008")
+console.log("That is a growth rate of approximatley " + (africa18-africa08) + "kg CO2")
 
-console.log("South America produces" + southAmerica18 + "kg CO2")
-console.log("South America contributes relatively" + Math.round(southAmerica18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("South America's emission from 2018 has increased by" + Math.round(southAmerica18/southAmerica08*100)  + "since 2008")
-console.log("That is a growth rate of approximatley" + (southAmerica18-southAmerica08) + "kg CO2")
+console.log("South America produces " + southAmerica18 + "kg CO2")
+console.log("South America contributes relatively " + Math.round(southAmerica18/total*100) + "% Carbon Dioxide to the world's total emission")
+console.log("South America's emission from 2018 has increased by " + Math.round(southAmerica18/southAmerica08*100)  + " since 2008")
+console.log("That is a growth rate of approximatley " + (southAmerica18-southAmerica08) + "kg CO2")
 
-console.log("Europe produces" +  europe18 + "kg CO2")
+console.log("Europe produces " +  europe18 + "kg CO2")
 console.log("Europe contributes relatively " +  Math.round(europe18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("Europe's emission from 2018 has increased by " +  Math.round(europe18/europe08*100)  + "since 2008")
+console.log("Europe's emission from 2018 has increased by " +  Math.round(europe18/europe08*100)  + " since 2008")
 console.log("That is a growth rate of approximatley " +  (europe18-europe08) + "kg CO2")
 
 console.log("North America produces " + northAmerica18 + "kg CO2")
 console.log("North America contributes relatively " +  Math.round(northAmerica18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("North America's emission from 2018 has increased by " +  Math.round(northAmerica18/northAmerica08*100)  + "since 2008")
+console.log("North America's emission from 2018 has increased by " +  Math.round(northAmerica18/northAmerica08*100)  + " since 2008")
 console.log("That is a growth rate of approximatley " +  (northAmerica18-northAmerica08) + "kg CO2")
  
 console.log("Asia produces " +  asia18 + "kg CO2")
 console.log("Asia contributes relatively " +  Math.round(asia18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("Asia's emission from 2018 has increased by " +  Math.round(asia18/asia08*100)  + "since 2008")
+console.log("Asia's emission from 2018 has increased by " +  Math.round(asia18/asia08*100)  + " since 2008")
 console.log("That is a growth rate of approximatley " +  (asia18-asia08) + "kg CO2")
 
 console.log("Australia produces "  +  australia18 + "kg CO2")
 console.log("Australia contributes relatively "  +  Math.round(australia18/total*100) + "% Carbon Dioxide to the world's total emission")
-console.log("Australia's emission from 2018 has increased by "  +  Math.round(australia18/australia08*100)  + "since 2008")
+console.log("Australia's emission from 2018 has increased by "  +  Math.round(australia18/australia08*100)  + " since 2008")
 console.log("That is a growth rate of approximatley "  +  (australia18-australia08) + "kg CO2")
 
